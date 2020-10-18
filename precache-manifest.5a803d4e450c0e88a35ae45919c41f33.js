e2adb0bf4a16eb859de86c1e56097549d53d9281
nazim <nazimaitali88@gmail.com>
2020-10-18 10:52:49 +0100

    Updates

---
+++ precache-manifest.5a803d4e450c0e88a35ae45919c41f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f4e70a5c3218a1e92931fc79de1a63",
+    "url": "/react-router-checkpoint/index.html"
+  },
+  {
+    "revision": "5ec598e816765771427f",
+    "url": "/react-router-checkpoint/static/css/main.e9e9a8d0.chunk.css"
+  },
+  {
+    "revision": "cf220f19bd1116cba073",
+    "url": "/react-router-checkpoint/static/js/2.db53d0be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-router-checkpoint/static/js/2.db53d0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec598e816765771427f",
+    "url": "/react-router-checkpoint/static/js/main.116ef876.chunk.js"
+  },
+  {
+    "revision": "278befc80a2cb9b42af5",
+    "url": "/react-router-checkpoint/static/js/runtime-main.028ccf89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-router-checkpoint.git/precache-manifest.059d1952dd8182fd77d6f23065b10668.js"
+  "/react-router-checkpoint/precache-manifest.5a803d4e450c0e88a35ae45919c41f33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-router-checkpoint.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-router-checkpoint/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
